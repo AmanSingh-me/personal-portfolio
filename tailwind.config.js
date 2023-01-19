6d3a87b092e5eeec50ab676814b6ad99bf4789dc
Aman Singh <amansingh24351@gmail.com>
2023-01-19 10:01:04 +0530

    Initial structure setup for portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "white-300": "#c1cfdc",
+      green: "#28a745",
+      teal: "#20c997",
+      "light-black": "#111418",
+      "black-900": "#000000e6",
+      blue: "#0596ec",
+      "sky-blue": "#000fff"
+    }
   },
   plugins: [],
-}
+};
