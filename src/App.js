a93fa8641685666e6838e7399b6b291c29fd3498
Aman Singh <amansingh24351@gmail.com>
2023-01-26 16:58:48 +0530

    changed education page to Contact Page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Main from "./components/Main";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
-import Education from "./pages/Education";
 import MyWork from "./pages/MyWork";
 import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
 import ErrorPage from "./pages/ErrorPage.js";
 
 function App() {
@@ -18,8 +18,8 @@ function App() {
           <Route path="/about" element={<About/>} > </Route>
           <Route path="/skills" element={<Skills/>}> </Route>
           <Route path="/mywork" element={<MyWork/>}> </Route>
-          <Route path="/education" element={<Education/>}> </Route>
           <Route path="/blogs" element={<Blogs/>}> </Route>
+          <Route path="/contact" element={<Contact/>}> </Route>
           <Route path="*" element={<ErrorPage/>}> </Route>
         </Routes> */}
         <Header />
--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -10,8 +10,8 @@ export default function NavBar() {
             { name: "About", li_Style: "my-4", path: "/about", link_Style: "text-xl hover:text-green" },
             { name: "Skills", li_Style: "my-4", path: "/skills", link_Style: "text-xl hover:text-green" },
             { name: "My Work", li_Style: "my-4", path: "/mywork", link_Style: "text-xl hover:text-green" },
-            { name: "Education", li_Style: "my-4", path: "/education", link_Style: "text-xl hover:text-green" },
-            { name: "Blogs", li_Style: "my-4", path: "/blogs", link_Style: "text-xl hover:text-green" }
+            { name: "Blogs", li_Style: "my-4", path: "/blogs", link_Style: "text-xl hover:text-green" },
+            { name: "Contact", li_Style: "my-4", path: "/contact", link_Style: "text-xl hover:text-green" }
           ].map(
             (details, index) => <li className={details.li_Style} key={index} >
                                 <Link to={details.path} className={details.link_Style}>{details.name}</Link>
